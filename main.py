5bbeaca402b8ce37c5b91256ba755e56bf59af99
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2023-08-05 14:12:34 +0200

    adding clear_terminal functions for graphs and pixels

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ def main():
         interfaces.show_interface(interfaces.main_interface)
         user_choice = interfaces.prompt_user(interfaces.main_interface.keys())
         if user_choice == 'G':
+            clear_terminal()
             interfaces.show_interface_prompt_user_and_call_function(interface=interfaces.graphs_interface)
             input('PRESS ANY KEY TO BACK TO MAIN MENU')
         elif user_choice == 'P':
+            clear_terminal()
             interfaces.show_interface_prompt_user_and_call_function(interface=interfaces.pixels_interface)
             input('PRESS ANY KEY TO BACK TO MAIN MENU')
         elif user_choice == 'Q':
             interfaces.exit_program()
+
 
 
 if __name__ == '__main__':

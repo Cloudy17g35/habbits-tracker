c2a5bc9ceb08da6dc761e7e4679e0c3836930496
Ksawery Lejczak <ksawery.lejczak@gmail.com>
2023-08-05 09:20:10 +0200

    adding interfaces to main file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
-from src.api_methods import graphs
+import warnings
+from src.api_methods import graphs, pixels
+from typing import Iterable
+
+
+warnings.simplefilter("ignore")
 
 def exit_program():
     sys.exit()
+
+
+def show_interface(interface:dict):
+    print('Here are possible methods')
+    for method in interface.values():
+        print(f'{method["label"]}: {method["description"]}')
 
 
 graphs_interface = {
@@ -28,21 +39,72 @@ graphs_interface = {
     }
 }
 
+pixels_interface = {
+    'C': {
+        'label': 'C',
+        'description': 'Create new pixel',
+        'callable': pixels.post_pixel
+    },
+    'G': {
+        'label': 'G',
+        'description': 'Get pixel',
+        'callable': pixels.get_pixel
+    },
+    'U': {
+        'label': 'U',
+        'description': 'Update pixel',
+        'callable': pixels.update_pixel
+    },
+    'D': {
+        'label': 'D',
+        'description': 'Delete pixel',
+        'callable': pixels.delete_pixel
+    }
+}
 
-def show_graphs_interface_for_user(graphs_interface:dict):
-    print('Here are possible methods:')
-    for method in graphs_interface.values():
-        print(f'{method["label"]}: {method["description"]}')
+
+main_interface = {
+    'G': {
+        'label': 'G',
+        'description': 'go to graphs interface',
+        'callable': show_interface
+    },
+    'P': {
+        'label': 'P',
+        'description': 'go to pixels interface',
+        'callable': show_interface
+    },
+    'Q': {
+            'label': 'Q',
+            'description': 'Exit program',
+            'callable': exit_program
+    }
+}
+
+
+def prompt_user(valid_options:Iterable):
+    user_choice = input("Select option:> ").upper()
+    while user_choice not in valid_options:
+        user_choice = input("Select option:> ").upper()
+    return user_choice
 
 
 def main():
     while True:
-        show_graphs_interface_for_user(graphs_interface)
-        user_choice = input("Please choose one of possible methods:").upper()
-        while user_choice.upper() not in graphs_interface.keys():
-            user_choice = input("Please choose one of possible methods:").upper()
-        function_to_call = graphs_interface[user_choice]['callable']
-        function_to_call()
+        show_interface(main_interface)
+        user_choice = prompt_user(main_interface.keys())
+        if user_choice == 'G':
+            show_interface(graphs_interface)
+            user_choice = prompt_user(graphs_interface.keys())
+            function_to_call = graphs_interface[user_choice]['callable']
+            function_to_call()
+        elif user_choice == 'P':
+            show_interface(pixels_interface)
+            user_choice = prompt_user(pixels_interface.keys())
+            function_to_call = pixels_interface[user_choice]['callable']
+            function_to_call()
+        elif user_choice == 'Q':
+            exit_program()
 
 
 if __name__ == '__main__':
